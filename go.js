545b8345d7dc29bc216596aa77708911251efbe7
Seaborn Lee <seabornlee.cn@gmail.com>
2017-08-10 18:40:56 +0800

    Execute ffmpeg commands to edit videos

---
+++ go.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const { exec } = require('child_process');
+
+let readConfig = (fileName) => {
+  return fs.readFileSync(fileName).toString().trim().split("\r\n");
+};
+
+let parse = (config) => {
+  let array = config.split("|");
+  return {
+    "name": array[0].trim(),
+    "ss": array[1].trim(),
+    "to": array[2].trim()
+  };
+}
+
+let toShellCommands = (configs) => {
+  return configs.map(c => {
+    let config = parse(c);
+    return `ffmpeg -i "${config.name}" -i logo.png -filter_complex 'overlay=main_w-overlay_w-40:main_h-overlay_h-60' -ss ${config.ss} -to ${config.to} -f mp4 "./Edited/${config.name}"`;
+  });
+}
+
+let execute = (shellCommands) => {
+  shellCommands.forEach(cmd => {
+    exec(cmd, (err, stdout) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      console.log(stdout);
+    })
+  });
+}
+
+let configs = readConfig("config.cfg");
+let shellCommands = toShellCommands(configs);
+execute(shellCommands);
--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const program = require("commander");
 program
   .version("1.0.0")
   .command("init", "Init the config file for editting")
+  .command("go", "Run ffmpeg with config")
   .parse(process.argv);
